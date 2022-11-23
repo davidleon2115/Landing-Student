175be7cf82c38e8568cbd50783d3c9edb876669b
Luis Almeida <114677081+LeeDaeMin@users.noreply.github.com>
2022-11-23 13:40:54 -0500

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,17 +1,15 @@
 <template>
+
   <div class="bg-[#1C233A] min-h-screen flex flex-col ">
     <div class="container mx-auto min-h-screen flex flex-col">
       <img src="~~/assets/img/nav.png" class="my-9" alt="">
       <div class="flex justify-items-start items-center">
         <div class="min-w-[300px] w-4/6 flex flex-col">
           <img class="mr-4" src="~~/assets/img/Video.png" alt="">
-          <div class=" w-[300px] ">
+          <div class=" min-w-[300px] w-5/6 ">
             <AdditionalContent/>
           </div>
         </div>
-
-        <div class="self-start ">
-
         <div class="self-start w-4/12">
           <comment/>
           <div class="p-2 flex flex-col">
@@ -20,7 +18,6 @@
             </div>
           </div>
         </div>
-
       </div>
     </div>
   </div>
@@ -33,10 +30,9 @@ import ShowComment from "./components/ShowCommet/ShowComment";
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700;900&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700;900&display=swap');
 
-body{
-    font-family: 'Montserrat', sans-serif;
-}
+    body{
+        font-family: 'Montserrat', sans-serif;
+    }
 </style>
-
